eb9d365a9105ff3f4429785c914b86e5b87ad7c1
Jacob Peroutek <JPeroutek@gmail.com>
2016-09-11 20:05:26 -0600

    Add rudimentary DataFetching. Test with `python DataFetch.py`

---
+++ DataFetch.py
@@ -0,0 +1,23 @@
+import json
+import requests
+
+def loadForFile(filename):
+    f = open(filename, "r")
+    dat = json.load(f)
+    f.close()
+    return dat
+
+if __name__ == "__main__":
+    items = loadForFile("./endpoints/BTC.json")
+    for i in items:
+        j = requests.get(i).json()
+        found = False
+        for l in ['last', 'last_price',]:
+            if l in j:
+                found = True
+                print(j[l])
+        if not found:
+            for k in ['btc_usd', 'USDT_BTC']:
+                if k in j:
+                    print(j[k]['last'])
+        raw_input()
