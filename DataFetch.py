4efb566fffc7ae80f489d4e0ae60d46ea351feef
Jacob Peroutek <JPeroutek@gmail.com>
2016-09-14 23:37:45 -0600

    Add new function DataFetch.pullPriceDataForSymbol(sym), and add a testing module

--- DataFetch.py
+++ DataFetch.py
@@ -1,3 +1,4 @@
+import config
 import json
 import requests
 
@@ -6,6 +7,34 @@ def loadForFile(filename):
     dat = json.load(f)
     f.close()
     return dat
+
+def pullPriceDataForSymbol(sym):
+    '''
+    sym = Symbol for Conversion.  Because all currencies are converted in 
+              relation to Bitcoin, we are just going to pull XXXBTC.json,
+              where `xxx` is the symbol parameter.
+    '''
+
+    if not sym in config.SUPPORTED_SYMBOLS:
+        return None
+
+    filename = "./endpoints/" + sym + "BTC.json"
+    ret = []
+    try:
+        dat = loadForFile(filename)
+    except Exception:
+        print("Failed to load endpoints for symbol " + sym)
+        return None
+    for endpoint in dat:
+        if config.DEBUG:
+            print("Fetching for " + endpoint["name"])
+        j = requests.get(endpoint["address"]).json()
+        pr = j
+        for i in range(len(endpoint["location"])):
+            pr = pr[endpoint["location"][i]]
+        pr = float(pr) ** endpoint["power"]
+        ret.append(pr)
+    return ret
 
 if __name__ == "__main__":
     items = loadForFile("./endpoints/BTC-USD.json")
---
+++ DataFetchTest.py
@@ -0,0 +1,3 @@
+# Run as python DataFetchTest.py
+import DataFetch
+print(DataFetch.pullPriceDataForSymbol('USD'))
